ced1e1fa223f08b827ef285b27277957589d6364
s1280155 <s1280155@std4dc14>
2021-07-02 12:06:19 +0900

    Greetings added

--- DiceGame.py
+++ DiceGame.py
@@ -4,6 +4,11 @@ dice1 = r.randint(1,6)
 dice2 = r.randint(1,6)
 total = dice1+dice2
 
+print('What is your name?')
+print('>',end='')
+Name = input()
+print('Hello,{}!'.format(Name))
+
 print('Rolling the dice...')
 print('Die1 :{}'.format(dice1))
 print('Die2 :{}'.format(dice2))
